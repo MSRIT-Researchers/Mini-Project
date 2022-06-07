9a3c684bc7acf6b17a0fe8fa43f170403fc6b525
aakash <p.saiaakash518@gmail.com>
2022-06-07 09:21:04 +0530

    added crow for server

---
+++ BPlusTree/server.cc
@@ -0,0 +1,15 @@
+#include "crow_all.h"
+//#include "crow_all.h"
+
+int main()
+{
+    crow::SimpleApp app; //define your crow application
+
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    //set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
+}
