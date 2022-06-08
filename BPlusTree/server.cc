99d642c9a471d0a2e81d468b3812687ac344793e
aakash <p.saiaakash518@gmail.com>
2022-06-08 19:25:57 +0530

    websocket server up and running

--- BPlusTree/server.cc
+++ BPlusTree/server.cc
@@ -1,5 +1,5 @@
 #include "crow_all.h"
-//#include "crow_all.h"
+#include <iostream>
 
 int main()
 {
@@ -9,6 +9,21 @@ int main()
     CROW_ROUTE(app, "/")([](){
         return "Hello world";
     });
+
+    // Websoket server
+    CROW_WEBSOCKET_ROUTE(app, "/ws")
+        .onopen([&](crow::websocket::connection& conn){
+                // Called when websocket server is launched.
+                std::cout<<"Websocket server running"<<std::endl;
+                })
+        .onclose([&](crow::websocket::connection& conn, const std::string& reason){
+                // Called when websocket server is closed.
+                std::cout<<"Wwbsocket server terminated"<<std::endl;
+                })
+        .onmessage([&](crow::websocket::connection& /*conn*/, const std::string& data, bool is_binary){
+                // Called when websocket server receives a message.
+                    std::cout<<data<<std::endl;
+                });
 
     //set the port, set the app to run on multiple threads, and run the app
     app.port(18080).multithreaded().run();
