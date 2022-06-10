4fea8502255335857b970881f56d5470d35f16a1
Soundarya03 <soundarya1103@gmail.com>
2022-06-10 22:18:16 +0530

    Adds thread_offset method to multithreading.cc

--- BPlusTree/multithreading.cc
+++ BPlusTree/multithreading.cc
@@ -1,12 +1,11 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string>
-#include<thread>
-#include<vector>
-#include<algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <thread>
+#include <vector>
+#include <algorithm>
 #include "bpt.h"
 #include "variables.h"
-
 
 std::pair<long long, long long> threadResults[THREAD_NUM];
 
@@ -14,52 +13,78 @@ void multithread(int left, int right, const int threadNumber);
 
 using namespace bpt;
 
-int main(void){
+int main(void)
+{
 
     bplus_tree database(DB_NAME);
     std::thread threads[THREAD_NUM];
     std::string str;
 
-    for(int i=0; i<THREAD_NUM; ++i){
-        threads[i] = std::thread (multithread, RANGE*i, RANGE*i+RANGE-1, i);
+    for (int i = 0; i < THREAD_NUM; ++i)
+    {
+        threads[i] = std::thread(multithread, RANGE * i, RANGE * i + RANGE - 1, i);
     }
 
-    for(int i=0; i<THREAD_NUM; ++i){
-        if(threads[i].joinable())
-            threads[i].join(); 
+    for (int i = 0; i < THREAD_NUM; ++i)
+    {
+        if (threads[i].joinable())
+            threads[i].join();
         printf("%lld %lld\n", threadResults[i].first, threadResults[i].second);
     }
-    
+
     return 0;
 }
 
-void multithread(int left, int right, const int threadNumber){
-    
-    
+void multithread(int left, int right, const int threadNumber)
+{
+
     std::string str = std::to_string(left);
     char leftStr[str.length()];
     strcpy(leftStr, str.c_str());
     str = std::to_string(right);
     char rightStr[str.size()];
-    strcpy(rightStr,str.c_str());
-    
-    
+    strcpy(rightStr, str.c_str());
+
     bplus_tree database(DB_NAME);
 
     bpt::key_t start(leftStr);
     value_t values[512];
     bool next = true;
     long long sum = 0;
-    long long c=0;
-    while (next) {
-        int ret = database.search_range(&start,rightStr, values, 512, &next);
+    long long c = 0;
+    while (next)
+    {
+        int ret = database.search_range(&start, rightStr, values, 512, &next);
         if (ret < 0)
-                break;
+            break;
 
-        for (int i = 0; i < ret; i++){
+        for (int i = 0; i < ret; i++)
+        {
             c++;
-            sum+=values[i];
+            sum += values[i];
         }
     }
     threadResults[threadNumber] = {sum, c};
 }
+
+/*
+void multithread_aggregate(const int thread_number, off_t start_leaf_offset, off_t end_leaf_offset = 0)
+{
+    long long sum = 0;
+    long long c = 0;
+
+    bpt::leaf_node_t temp;
+    bpt::bplus_tree::map(&temp, temp.prev);
+    while (temp.next != end_leaf_offset)
+    {
+        for (int i = 0; i < temp.n; ++i)
+        {
+            sum += temp.children[i].value;
+            c++;
+        }
+        bpt::bplus_tree::map(&temp, temp.next);
+    }
+
+    threadResults[thread_number] = {sum, c};
+}
+*/
